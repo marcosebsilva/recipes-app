a4fe38d6b5fbe262c52a15f2e71cf7674bfe1476
Marcos Estevao <dusdoom@gmail.com>
2021-09-21 19:01:09 -0300

    added requirement 15

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RecipesProvider from './provider/RecipesProvider';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <RecipesProvider>
       <p>Inimigos do Redux</p>
+      <SearchBar />
     </RecipesProvider>
   );
 }
