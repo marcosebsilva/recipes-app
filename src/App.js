d11055955070413708126e0eb8397e37cdd65785
Marcos Estevao <dusdoom@gmail.com>
2021-09-21 20:02:18 -0300

    fix lint + comentarios para ajudar no merge

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RecipesProvider from './provider/RecipesProvider';
-import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <RecipesProvider>
       <p>Inimigos do Redux</p>
-      <SearchBar />
     </RecipesProvider>
   );
 }
