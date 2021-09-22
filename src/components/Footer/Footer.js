0b6df179342f0f7e983fc911bc894dd0a867dc86
PedroSpelta <p.h.s.8989@hotmail.com>
2021-09-22 00:53:09 -0300

    req 20 completo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { ImCompass2, ImGlass } from 'react-icons/im';
-import { IoFastFoodOutline } from 'react-icons/io5';
+import './style.css';
+import drinksImg from '../../images/drinkIcon.svg';
+import exploreImg from '../../images/exploreIcon.svg';
+import mealImg from '../../images/mealIcon.svg';
 
 function Footer() {
   return (
-    <div data-testid="footer">
-      <ImGlass data-testid="drinks-bottom-btn" />
-      <ImCompass2 data-testid="explore-bottom-btn" />
-      <IoFastFoodOutline data-testid="food-bottom-btn" />
+    <div data-testid="footer" className="footer">
+      <img
+        src={ drinksImg }
+        alt="drinks"
+        data-testid="drinks-bottom-btn"
+      />
+      <img
+        src={ exploreImg }
+        alt="explore"
+        data-testid="explore-bottom-btn"
+      />
+      <img
+        src={ mealImg }
+        alt="food"
+        data-testid="food-bottom-btn"
+      />
     </div>
   );
 }
