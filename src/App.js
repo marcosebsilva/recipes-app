30a35ea9a2c48397a4343d02f28477fc7b44aa62
giiffonseca <guii.ffonseca22@gmail.com>
2021-09-22 16:08:42 -0300

    Requisitos 05 06 07 08

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RecipesProvider from './provider/RecipesProvider';
+import Login from './components/Login';
+import Comidas from './components/Comidas';
 
 function App() {
   return (
     <RecipesProvider>
-      <p>Inimigos do Redux</p>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/comidas" component={ Comidas } />
+      </Switch>
     </RecipesProvider>
   );
 }
