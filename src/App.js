db3e3d593f65361ee53c52b12f494f76b56f890b
Vinicius <vinicius.10.carvalho.100@gmail.com>
2021-09-22 20:33:29 -0300

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RecipesProvider from './provider/RecipesProvider';
-import Comidas from './components/Comidas';
-import Login from './components/Login';
+import Routes from './Routes';
 
 function App() {
   return (
     <RecipesProvider>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route path="/comidas" component={ Comidas } />
-      </Switch>
+      <Routes />
     </RecipesProvider>
   );
 }
