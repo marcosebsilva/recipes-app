5a64e94aa559e0d9d45d8c46ee52768401a37cc0
Marcos Estevao <dusdoom@gmail.com>
2021-09-23 17:17:03 -0300

    added fetch of food by id

--- src/pages/EmProgresso.js
+++ src/pages/EmProgresso.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouteMatch } from 'react-router';
 
 export default function EmProgresso() {
+  const match = useRouteMatch();
+  const [recipe, setRecipe] = useState();
+
+  async function getRecipe() {
+    const foodId = match.params.id;
+    const response = await (await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodId}`)).json();
+    try {
+      const food = Object.values(response)[0][0];
+      setRecipe(food);
+    } catch (error) {
+      alert('Esse rango ai nao existe');
+    }
+  }
+
+  useEffect(() => {
+    getRecipe();
+  }, []);
+
   return (
-    <div>manda salve</div>
+    <div>
+      manda salve
+      {recipe && recipe.strMeal}
+    </div>
   );
 }
