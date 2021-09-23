12eac4cc742dc89e8c39e3dc401c3116d1a11037
PedroSpelta <p.h.s.8989@hotmail.com>
2021-09-23 15:53:31 -0300

    Terminando de dar merge nos requisitos anteriores

--- src/pages/Comidas.js
+++ src/pages/Comidas.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../components/Footer/Footer';
 import Header from '../components/Header';
 
 export default function Comidas() {
   return (
     <div>
       <Header title="Comidas" />
+      <Footer />
     </div>
   );
 }
