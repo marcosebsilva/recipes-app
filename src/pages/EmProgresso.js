952712302f755d264c6a58c69e0cfae6372077b1
Marcos Estevao <dusdoom@gmail.com>
2021-09-23 17:32:30 -0300

    refactored fetch to useCallback

--- src/pages/EmProgresso.js
+++ src/pages/EmProgresso.js
@@ -1,29 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import { useRouteMatch } from 'react-router';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useRouteMatch, useLocation } from 'react-router';
 
 export default function EmProgresso() {
+  const location = useLocation();
   const match = useRouteMatch();
+
   const [recipe, setRecipe] = useState();
 
-  async function getRecipe() {
+  const api = location.pathname.includes('/bebidas')
+    ? 'thecocktaildb'
+    : 'themealdb';
+
+  const foodType = {
+    thecocktaildb: 'Drink',
+    themealdb: 'Meal',
+  };
+
+  const getRecipeMemo = useCallback(async () => {
     const foodId = match.params.id;
-    const response = await (await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodId}`)).json();
+    const response = await (await fetch(`https://www.${api}.com/api/json/v1/1/lookup.php?i=${foodId}`)).json();
     try {
       const food = Object.values(response)[0][0];
       setRecipe(food);
     } catch (error) {
       alert('Esse rango ai nao existe');
     }
-  }
+  }, [api, match]);
 
   useEffect(() => {
-    getRecipe();
-  }, []);
+    getRecipeMemo();
+  }, [getRecipeMemo]);
 
   return (
     <div>
-      manda salve
-      {recipe && recipe.strMeal}
+      Manda salve
+      {recipe && recipe[`str${foodType[api]}`] }
     </div>
   );
 }
