5b5a1420a4cccc4793f2dae3009743bc45c81b34
PedroSpelta <p.h.s.8989@hotmail.com>
2021-09-26 20:39:33 -0300

    terminando os requisitos do explore

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,13 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import Login from './pages/Login';
 import Comidas from './pages/Comidas';
 import Bebidas from './pages/Bebidas';
-<<<<<<< HEAD
-import Explore from './pages/Explore';
-
-// import Bebidas from './pages/Bebidas';
-// import ComidasDetails from './pages/ComidasDetails';
-// import BebidasDetails from './pages/BebidasDetails';
-=======
 import EmProgresso from './pages/EmProgresso';
 import Perfil from './pages/Perfil';
 import ComidasDetails from './pages/ComidasDetails';
@@ -23,7 +16,6 @@ import ExplorarBebidasIngredientes from './pages/ExplorarBebidasIngredientes';
 import ExplorarComidasLocal from './pages/ExplorarComidasLocal';
 import ReceitasFeitas from './pages/ReceitasFeitas';
 import ReceitasFavoritas from './pages/ReceitasFavoritas';
->>>>>>> 34f5ef9a0fdd05107f8e6a1093c35ae0423a1c9c
 
 function Routes() {
   return (
@@ -31,21 +23,6 @@ function Routes() {
       <Switch>
         <Route exact path="/comidas" component={ Comidas } />
         <Route exact path="/bebidas" component={ Bebidas } />
-<<<<<<< HEAD
-        <Route exact path="/comidas/:id" />
-        <Route exact path="/bebidas/:id" />
-        <Route exact path="/comidas/:id/in-progress" />
-        <Route exact path="/bebidas/:id/in-progress" />
-        <Route exact path="/explorar" component={ Explore } />
-        <Route exact path="/explorar/comidas" />
-        <Route exact path="/explorar/bebidas" />
-        <Route exact path="/explorar/comidas/ingredientes" />
-        <Route exact path="/explorar/bebidas/ingredientes" />
-        <Route exact path="/explorar/comidas/area" />
-        <Route exact path="/perfil" />
-        <Route exact path="/receitas-feitas" />
-        <Route exact path="/receitas-favoritas" />
-=======
         <Route exact path="/comidas/:id" component={ ComidasDetails } />
         <Route exact path="/bebidas/:id" component={ BebidasDetails } />
         <Route exact path="/comidas/:id/in-progress" component={ EmProgresso } />
@@ -67,7 +44,6 @@ function Routes() {
         <Route exact path="/perfil" component={ Perfil } />
         <Route exact path="/receitas-feitas" component={ ReceitasFeitas } />
         <Route exact path="/receitas-favoritas" component={ ReceitasFavoritas } />
->>>>>>> 34f5ef9a0fdd05107f8e6a1093c35ae0423a1c9c
         <Route path="/" component={ Login } />
       </Switch>
     </BrowserRouter>
--- src/pages/ExplorarComida.jsx
+++ src/pages/ExplorarComida.jsx
@@ -1,11 +1,56 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+import FetchAPI from '../components/FetchAPI';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function ExplorarComidas() {
+  const history = useHistory();
+
+  async function fetchRandomFoodId() {
+    const meals = await FetchAPI('https://www.themealdb.com/api/json/v1/1/random.php');
+    const randomMeal = meals.meals[0];
+    const { idMeal } = randomMeal;
+    return idMeal;
+  }
+
+  async function clickExplore(e) {
+    const { name } = e.target;
+    if (name === 'surpresa') {
+      const randomId = await fetchRandomFoodId();
+      history.push(`/comidas/${randomId}`);
+      return;
+    }
+    history.push(`/explorar/comidas/${name}`);
+  }
+
   return (
     <div>
       <Header title="Explorar Comidas" />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        name="ingredientes"
+        onClick={ clickExplore }
+      >
+        Por Ingredientes
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-area"
+        name="area"
+        onClick={ clickExplore }
+      >
+        Por Local de Origem
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        name="surpresa"
+        onClick={ clickExplore }
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </div>
   );
